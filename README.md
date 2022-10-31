8cdb5e5319d5e1881869273e724beace888beec3
pwoogi <chrishw1029@gmail.com>
2022-10-31 20:55:37 +0900

    singleton, adapter pattern 복습

---
+++ README.md
@@ -0,0 +1,11 @@
+# Design Pattern
+
+<br />
+
+## Singleton pattern
+* Singleton 패턴은 어떠한 클래스(객체)가 유일하게 1개만 존재할 때 사용
+* 실물 세계 = 프린터, 실제 프로그래밍 = TCP Socket 통신에서 서버와 연결된 connect 객체
+
+## Adapter pattern
+* SOLID - OCP 원칙
+* 호환성이 없는 기존 클래스의 인터페이스를 변환하여 재사용 할 수 있게 함
--- src/com/company/design/Main.java
+++ src/com/company/design/Main.java
@@ -1,5 +1,6 @@
 package com.company.design;
 
+import com.company.design.adapter.*;
 import com.company.design.singleton.AClazz;
 import com.company.design.singleton.BClazz;
 import com.company.design.singleton.SocketClient;
@@ -8,13 +9,29 @@ public class Main {
 
     public static void main(String[] args) {
 
-        AClazz aClazz = new AClazz();
-        BClazz bClazz = new BClazz();
+//        AClazz aClazz = new AClazz();
+//        BClazz bClazz = new BClazz();
+//
+//        SocketClient aClient = aClazz.getSocketClient();
+//        SocketClient bClient = bClazz.getSocketClient();
+//
+//        System.out.println("두개의 객체는 동일한가?");
+//        System.out.println(aClient.equals(bClient));
 
-        SocketClient aClient = aClazz.getSocketClient();
-        SocketClient bClient = bClazz.getSocketClient();
+        HairDryer hairDryer = new HairDryer();
+        connect(hairDryer);
 
-        System.out.println("두개의 객체는 동일한가?");
-        System.out.println(aClient.equals(bClient));
+        Cleaner cleaner = new Cleaner();
+        Electronic110V adapter = new SocketAdapter(cleaner);
+        connect(adapter);
+
+        AirConditioner airConditioner = new AirConditioner();
+        Electronic110V airAdapter = new SocketAdapter(airConditioner);
+        connect(airAdapter);
+    }
+
+    //콘센트
+    public static void connect(Electronic110V electronic110V){
+        electronic110V.powerOn();
     }
 }
