1d5cd8fbb94b9af7d0b5bf174872daab5bbc3f3c
pwoogi <chrishw1029@gmail.com>
2022-10-31 21:27:19 +0900

    Proxy 패턴에서 cache 활용, AOP 간단하게 복습

--- README.md
+++ README.md
@@ -9,3 +9,8 @@
 ## Adapter pattern
 * SOLID - OCP 원칙
 * 호환성이 없는 기존 클래스의 인터페이스를 변환하여 재사용 할 수 있게 함
+
+## Proxy pattern
+* Proxy 패턴은 Proxy Class를 통해 대신해서 전달하는 형태로 설계되어 있음
+* 실제 Client는 Proxy로부터 결과를 전달받고 Cache의 기능으로도 활용되고 있음.
+* SOLID - OCP 원칙 & DIP 원칙
--- src/com/company/design/Main.java
+++ src/com/company/design/Main.java
@@ -1,9 +1,15 @@
 package com.company.design;
 
 import com.company.design.adapter.*;
+import com.company.design.aop.AopBrowser;
+import com.company.design.proxy.Browser;
+import com.company.design.proxy.BrowserProxy;
+import com.company.design.proxy.IBrowser;
 import com.company.design.singleton.AClazz;
 import com.company.design.singleton.BClazz;
 import com.company.design.singleton.SocketClient;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Main {
 
@@ -18,20 +24,55 @@ public class Main {
 //        System.out.println("두개의 객체는 동일한가?");
 //        System.out.println(aClient.equals(bClient));
 
-        HairDryer hairDryer = new HairDryer();
-        connect(hairDryer);
+//        HairDryer hairDryer = new HairDryer();
+//        connect(hairDryer);
+//
+//        Cleaner cleaner = new Cleaner();
+//        Electronic110V adapter = new SocketAdapter(cleaner);
+//        connect(adapter);
+//
+//        AirConditioner airConditioner = new AirConditioner();
+//        Electronic110V airAdapter = new SocketAdapter(airConditioner);
+//        connect(airAdapter);
+        /*
+        Browser browser = new Browser("www.naver.com");
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();
 
-        Cleaner cleaner = new Cleaner();
-        Electronic110V adapter = new SocketAdapter(cleaner);
-        connect(adapter);
+        IBrowser browser = new BrowserProxy("www.naver.com");
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();
 
-        AirConditioner airConditioner = new AirConditioner();
-        Electronic110V airAdapter = new SocketAdapter(airConditioner);
-        connect(airAdapter);
+         */
+
+        AtomicLong start = new AtomicLong();
+        AtomicLong end = new AtomicLong();
+
+        IBrowser aopBrowser = new AopBrowser("www.naver.com",
+                () -> {
+                    System.out.println("before");
+                    start.set(System.currentTimeMillis());
+                },
+                () -> {
+                    long now = System.currentTimeMillis();
+                    end.set(now - start.get());
+                }
+        );
+        aopBrowser.show();
+        System.out.println("loading time : " + end.get());
+
+        aopBrowser.show();
+        System.out.println("loading time : " + end.get());
     }
 
     //콘센트
-    public static void connect(Electronic110V electronic110V){
+    public static void connect(Electronic110V electronic110V) {
         electronic110V.powerOn();
     }
+
+
 }
